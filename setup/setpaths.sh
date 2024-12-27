fd01d95c8821c89640f6de626c94f601838e8950
Jean-Baptiste Queru <jbqueru@gmail.com>
2024-12-27 12:21:45 +0200

    Adjust paths to new location of tools

--- setup/setpaths.sh
+++ setup/setpaths.sh
@@ -1,15 +1,15 @@
 # Source this in your shell to get the paths set for the
 # various tools
 
-export PATH=$PATH:/home/jbq/code/emulators/hatari/src
-export PATH=$PATH:/home/jbq/code/emulators/hatari/tools/hmsa
-export PATH=$PATH:/home/jbq/code/emulators/caprice32
-export PATH=$PATH:/home/jbq/code/emulators/fuse-gtk
-export PATH=$PATH:/home/jbq/code/emulators/fuse-sdl
-export PATH=$PATH:/home/jbq/code/emulators/zesarux/src
-export PATH=$PATH:/home/jbq/code/emulators/CLK/OSBindings/SDL
-export PATH=$PATH:/home/jbq/code/emulators/fbzx/src
-# export PATH=$PATH:/home/jbq/code/emulators/oricutron
+export PATH=$PATH:/home/jbq/code/tools/hatari/src
+export PATH=$PATH:/home/jbq/code/tools/hatari/tools/hmsa
+export PATH=$PATH:/home/jbq/code/tools/caprice32
+export PATH=$PATH:/home/jbq/code/tools/fuse-gtk
+export PATH=$PATH:/home/jbq/code/tools/fuse-sdl
+export PATH=$PATH:/home/jbq/code/tools/zesarux/src
+export PATH=$PATH:/home/jbq/code/tools/CLK/OSBindings/SDL
+export PATH=$PATH:/home/jbq/code/tools/fbzx/src
+# export PATH=$PATH:/home/jbq/code/tools/oricutron
 export PATH=$PATH:/home/jbq/code/tools/rmac
 export PATH=$PATH:/home/jbq/code/tools/upx/build/release
 export PATH=$PATH:/home/jbq/code/tools/zasm
