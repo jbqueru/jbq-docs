56d2b6b22ec8687d25214f06b1cfccb95d2705fd
Jean-Baptiste Queru <jbqueru@gmail.com>
2025-04-26 18:51:03 +0300

    Add mtools to list of packages to be installed

--- setup/packages.sh
+++ setup/packages.sh
@@ -6,7 +6,7 @@
 mkdir -p ~/code/build
 
 export PACKAGES='curl build-essential golang-go
-  git cmake scons autoconf libtool flex bison
+  git cmake scons autoconf libtool flex bison mtools
   libsdl1.2-dev libsdl2-dev libpng-dev
   libfreetype-dev libgtk-3-dev libncurses-dev'
 
